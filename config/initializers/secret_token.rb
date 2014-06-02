86c725e84999b9c5550f5af822dc234eb9b11525
SoniaG2 <songonza@gmail.com>
2014-06-02 18:09:05 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Disscontrol::Application.config.secret_token = '34857016f760e34d9b80e4ed154fbde4819e23c302382ae1bfe0efc72ce119cfce64130172e8117e287ed73e7a0d00634cd096834f27cad68896a202d9fc2f13'
